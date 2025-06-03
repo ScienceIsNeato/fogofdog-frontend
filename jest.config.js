a6ebaab5c4611e57aeeb76418570d915bdf7f9f8
Will Martin <quarkswithforks@gmail.com>
2025-06-03 15:31:14 -0500

    Fix TestFlight submission CI failure - Add Node.js setup and dependency installation to resolve expo-location plugin

--- jest.config.js
+++ jest.config.js
@@ -34,10 +34,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 59,
-      functions: 65,
-      lines: 70,
-      statements: 70
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
     }
   },
   moduleNameMapper: {
