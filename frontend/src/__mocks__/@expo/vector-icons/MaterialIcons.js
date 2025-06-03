889a1c15b06fb6acda56594b23c3b88ad1165e9a
Will Martin <quarkswithforks@gmail.com>
2025-06-03 06:04:43 -0500

    feat: Complete speed-focused CI quality pipeline - Zero warnings, fast tests, security audit, no watchman issues

--- frontend/src/__mocks__/@expo/vector-icons/MaterialIcons.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-const MaterialIcons = ({ name, size, color, ...props }) => (
-  <Text {...props}>{name}</Text>
-);
-
-export default MaterialIcons; 
